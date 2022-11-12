4d7defa6c1938421f4406e8496c391424260c1a6
RizeKishimaro <jokerlove267@gmail.com>
2022-11-12 22:05:56 +0630

    Added dot files

---
+++ conky-themes/start.sh
@@ -0,0 +1,7 @@
+#bin/bash
+#script created by Rize Kishimaro
+conky -c /home/rize/Downloads/Compressed/conky-themes/Logo &
+conky -c  /home/rize/Downloads/Compressed/conky-themes/cpu &
+conky -c  /home/rize/Downloads/Compressed/conky-themes/mem &
+conky -c  /home/rize/Downloads/Compressed/conky-themes/disk &
+conky -c  /home/rize/Downloads/Compressed/conky-themes/procs 
